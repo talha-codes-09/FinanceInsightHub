810d0de83f56832429ec640791ed3a726022d1c0
talha-codes-09 <talhahassan456321@gmail.com>
2026-07-22 16:59:57 +0500

    Day 3: Added Transaction model and configured Entity Framework Core

---
+++ FinanceInsightHub/Models/Transation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceInsightHub.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, 100000000)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        public string TransactionType { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [StringLength(250)]
+        public string? Notes { get; set; }
+    }
+}
---
+++ FinanceInsightHub/data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinanceInsightHub.Models;
+
+namespace FinanceInsightHub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Transaction table
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
